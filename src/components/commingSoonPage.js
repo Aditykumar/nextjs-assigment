0b63a0e3039d361e5a49412256340834d7c32492
adity1999 <adity.kumar@collegedekho.com>
2023-07-16 19:03:32 +0530

    assignment complete

--- src/components/commingSoonPage.js
+++ src/components/commingSoonPage.js
@@ -14,7 +14,7 @@ const CommingSoonPage = () => {
       />
       <h2>Feature Coming Soon</h2>
       <p>
-        We are excited to announce that we will be launching soon and can't wait
+        We are excited to announce that we will be launching soon and can&apos;t wait
         to share our new platform with you.
       </p>
       <Link href={"/"}>Go To Home Page</Link>
